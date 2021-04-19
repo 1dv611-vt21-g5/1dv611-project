366cea2c0c89f25175cd94b0307901609a7e1e5e
Lisa Veltman Hellinger <lv222fz@student.lnu.se>
2021-04-19 13:01:58 +0200

    Created models and schemas

---
+++ backend/src/models/Setting.js
@@ -0,0 +1,33 @@
+/**
+ * Mongoose model Settings.
+ *
+ * @version 1.0.0
+ */
+
+'use strict'
+
+const mongoose = require('mongoose')
+
+// Create a schema.
+const settingsSchema = new mongoose.Schema({
+    node_id: {
+      type: String,
+      required: true
+    },
+    value_name: {
+      type: String,
+      required: true
+    },
+    interval: {
+      type: String,
+      required: true
+    }
+  }, {
+    timestamps: false
+  })
+  
+  // Create a model using the schema.
+  const Setting = mongoose.model('Setting', settingSchema)
+  
+  // Exports.
+  module.exports = Setting
