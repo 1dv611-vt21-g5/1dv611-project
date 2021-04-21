24408c47a0bed7fcb78c17c4442f325b78464353
Adam Karlsten <adamkarlsten@gmail.com>
2021-04-21 03:29:09 +0200

    More styling changes, adds a few TODO notes in comments

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -4,40 +4,45 @@
  * @version 1.0.0
  */
 
- 'use strict'
+'use strict'
 
- const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
- // Create a schema.
+// Create a schema.
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    api_key_zapier: {
-      type: String,
-      required: false,
-    },
-    access_token_yggio: {
-      type: String,
-      required: false
-    },
-    refresh_token_yggio: {
-      type: String,
-      required: false
-    },
-    expiresAt_token_yggio: {
-      type: String,
-      required: false
-    }
-  }, {
-    timestamps: false
-  })
+  username: {
+    type: String,
+    required: true
+  },
+  api_key_zapier: {
+    type: String,
+    required: false,
+  },
+  access_token_yggio: {
+    type: String,
+    required: false
+  },
+  refresh_token_yggio: {
+    type: String,
+    required: false
+  },
+  expiresAt_token_yggio: {
+    type: String,
+    required: false
+  }
+}, {
+  timestamps: false
+})
+
+// TODO: Add generation of API key when user is registered, also add endpoints
+// for removing current key and creating a new one (invalidating)
+// Is a random (uuid-type) string enough? Or do we need to hash some values?
+// If the API key is randomly generated it should be impossible to guess what
+// another users key is - but we can still check it via the database
 
 
-  // Create a model using the schema.
-  const User = mongoose.model('User', userSchema)
-  
-  // Exports.
-  module.exports = User
-  
+// Create a model using the schema.
+const User = mongoose.model('User', userSchema)
+
+// Exports.
+module.exports = User
--- frontend/components/DataBox.js
+++ frontend/components/DataBox.js
@@ -2,11 +2,11 @@ import { Box, Flex, Text } from '@chakra-ui/react'
 
 const DataBox = ({ setShowData, data }) => {
   return (
-    <Box onClick={() => setShowData(false)}>
+    <Box cursor="pointer" onClick={() => setShowData(false)}>
       {Object.entries(data).map(([key, value]) => (
         <Flex>
           <Text fontSize="xs" mr="0.5rem" fontWeight="bold">{key}:</Text>
-          <Text fontSize="xs">{JSON.stringify(value)}</Text>
+          <Text fontSize="xs" textAlign="left" maxW="40rem" isTruncated>{JSON.stringify(value)}</Text>
         </Flex>
       ))}
     </Box>
