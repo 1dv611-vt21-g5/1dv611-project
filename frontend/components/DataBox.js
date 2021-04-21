782a6600d7b3ebb85bdbe0454418a6c99cd269b6
Filippa Jakobsson <fj222nq@student.lnu.se>
2021-04-21 10:17:26 +0200

    fixing some errors

--- frontend/components/DataBox.js
+++ frontend/components/DataBox.js
@@ -2,15 +2,16 @@ import { Box, Flex, Text } from '@chakra-ui/react'
 
 const DataBox = ({ setShowData, data }) => {
   return (
-    <Box cursor="pointer" onClick={() => setShowData(false)}>
+    <Box cursor='pointer' onClick={() => setShowData(false)}>
       {Object.entries(data).map(([key, value]) => (
-        <Flex>
-          <Text fontSize="xs" mr="0.5rem" fontWeight="bold">{key}:</Text>
-          <Text fontSize="xs" textAlign="left" maxW="40rem" isTruncated>{JSON.stringify(value)}</Text>
+        <Flex key={key}>
+          <Text fontSize='xs' mr='0.5rem' fontWeight='bold'>{key}:</Text>
+          <Text fontSize='xs' textAlign='left' maxW='40rem' isTruncated>{JSON.stringify(value)}</Text>
         </Flex>
       ))}
     </Box>
   )
 }
 
+// Exports.
 export default DataBox
