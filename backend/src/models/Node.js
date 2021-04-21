128da9bab831cf31b49f42192c248343229efe7a
Filippa Jakobsson <fj222nq@student.lnu.se>
2021-04-21 09:25:40 +0200

    some refactoring

--- backend/src/models/Node.js
+++ backend/src/models/Node.js
@@ -10,25 +10,24 @@ const mongoose = require('mongoose')
 
 // Create a schema.
 const nodeSchema = new mongoose.Schema({
-   nodeType: {
-       type: String,
-       required: true
-   },
-   name: {
-     type: String,
-     required: false,
-   },
-   values: { // Not applicable for devices of type "zwave-device"
-      type: Array,
-      required: true
-   }
- }, {
-   timestamps: true
- })
- 
- // Create a model using the schema.
- const Node = mongoose.model('Node', nodeSchema)
- 
- // Exports.
- module.exports = Node
- 
+  nodeType: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: false
+  },
+  values: { // Not applicable for devices of type "zwave-device"
+    type: Array,
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+// Create a model using the schema.
+const Node = mongoose.model('Node', nodeSchema)
+
+// Exports.
+module.exports = Node
