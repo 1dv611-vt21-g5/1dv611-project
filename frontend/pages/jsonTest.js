d923ada47cb419fd9e48b98eaa13c7fbbe0cf1fd
Adam Karlsten <adamkarlsten@gmail.com>
2021-05-09 15:42:46 +0200

    Adds JSON tree viewer with some customizations

---
+++ frontend/pages/jsonTest.js
@@ -0,0 +1,12 @@
+// import Head from 'next/head'
+import Page from 'components/Page'
+import JsonPathPicker from 'components/JsonPath'
+
+
+export default function Home() {
+  return (
+    <Page>
+      <JsonPathPicker />
+    </Page>
+  )
+}
