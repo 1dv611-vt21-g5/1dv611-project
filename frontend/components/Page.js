5aa93dc448e6c78ce6194f089070edf0b98be8cd
Lisa Veltman Hellinger <lv222fz@student.lnu.se>
2021-05-18 16:23:37 +0200

    La till user context så att navbaren visar rätt innehåll

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -7,25 +7,25 @@ import { Flex, Spacer, Box } from '@chakra-ui/react'
 const Page = ({ children }) => {
 
   return (
-    <Flex flexDirection="column" className="App" bg="lime.background" minH="100vh">
-      <Head>
-        <title>Ysocial - Connect Yggio and Zapier</title>
-      </Head>
-      <Flex className="App-header">
-        <Link href="/devices">
-          <Box cursor="pointer">
-            <Image priority={true} src="/img/logo.png" className="App-logo" alt="logo" layout="intrinsic" width={188} height={72} />
-          </Box>
-        </Link>
+      <Flex flexDirection="column" className="App" bg="lime.background" minH="100vh">
+        <Head>
+          <title>Ysocial - Connect Yggio and Zapier</title>
+        </Head>
+        <Flex className="App-header">
+          <Link href="/devices">
+            <Box cursor="pointer">
+              <Image priority={true} src="/img/logo.png" className="App-logo" alt="logo" layout="intrinsic" width={188} height={72} />
+            </Box>
+          </Link>
+          <Spacer />
+          <Navbar />
+        </Flex>
+        {children}
         <Spacer />
-        <Navbar />
+        <footer className="App-footer">
+          <p>A project in course 1dv611 at Linneaus university. </p>
+        </footer>
       </Flex>
-      {children}
-      <Spacer />
-      <footer className="App-footer">
-        <p>A project in course 1dv611 at Linneaus university. </p>
-      </footer>
-    </Flex>
   )
 }
 
