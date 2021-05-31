bc2b397dbc6593c1aebd249895caeb2fdf4a942d
Adam Karlsten <adamkarlsten@gmail.com>
2021-05-31 15:01:15 +0200

    Adds note warning about OAuth secret

--- frontend/pages/devices.js
+++ frontend/pages/devices.js
@@ -3,11 +3,11 @@ import Page from 'components/Page'
 import DeviceList from 'components/DeviceList'
 import PleaseLogin from 'components/PleaseLogin'
 
-export default function Home () {
+export default function Home() {
   return (
     <Page>
       <PleaseLogin>
-      <DeviceList />
+        <DeviceList />
       </PleaseLogin>
     </Page>
   )
